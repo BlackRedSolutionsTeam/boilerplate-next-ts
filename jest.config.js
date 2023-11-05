43865c50b9669e805899fff64ea909873835dbb3
Vagner <vagner.araujo@csptech.com.br>
2023-11-05 14:43:51 -0300

    Correção jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/', '/.husky/'],
   collectCoverage: true,
-  collectCoverageFrom: ['app/**/*.ts(x)'],
+  collectCoverageFrom: ['app/**/*.ts(x)?', 'app/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
