107d09136dd93e57e9f474211127c0fb4a1b0f80
Vagner <vagner.araujo@csptech.com.br>
2023-11-05 14:11:39 -0300

    Adicionado storybook e pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = [nextConfig, withPWA]
